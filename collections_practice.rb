21a583cc350a62d30bc638bdf90ee47622395cb8
Allen F Partlow <allenfp@gmail.com>
2019-01-31 06:17:26 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -73,6 +73,10 @@ def find_cool(data)
 end
 
 def organize_schools(data)
-  data.invert.merge
+new_hash = Hash.new
+data[:schools].each do |key, value|
+  new_hash << value[:location] key
+end
+end
 
 end
