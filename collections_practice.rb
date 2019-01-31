9fcfea1ffa56268fd3417b145ef8bf3a23c4f075
Allen F Partlow <allenfp@gmail.com>
2019-01-31 03:44:57 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -51,10 +51,10 @@ def merge_data(keys, data)
 
   data.each do |data_element|
     keys.each do |key_element|
-  #     # data_element[key_element[:first_name]]["first_name"] = key_element[:first_name]
-  #     new_array.push() #data_element[key_element[:first_name]].values)
+      data_element[key_element[:first_name]]["first_name"] = key_element[:first_name]
+      new_array.push() #data_element[key_element[:first_name]].values)
     end
-  #   new_array
-  end
 
+  end
+    new_array
 end
